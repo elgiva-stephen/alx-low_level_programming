8fadab675d4104ed4ffd063d0dde4f123358de15
Elgiva Stephen <elgiva.stephen@gmail.com>
2022-10-17 18:01:35 -0700

    Write script that prints exactly, followed by a new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - prints and that piece of art is useful - Dora Korpa,
+ * 2015-10-19
  * Return : Always 0 (Successful)
  */
 
 int main(void)
 {
-	write(2, "and that peice of art useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
