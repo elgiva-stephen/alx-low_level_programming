f72ecbe6780fe0022d7473337e527842c546e3b7
Elgiva Stephen <elgiva.stephen@gmail.com>
2022-10-17 18:27:38 -0700

    Script that prints followed by new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - prints and that piece of art is useful - Dora Korpar, 2015-10-19
+ * Main - prints and that piece of art is useful - Dora Korpar, 2015-10-19
  * Return : Always 0 (Successful)
  */
 
