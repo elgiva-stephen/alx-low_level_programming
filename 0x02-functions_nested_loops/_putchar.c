0c815c70521b25a6d8b2c7d7349344c326a29ebf
Elgiva Stephen <elgiva.stephen@gmail.com>
2022-10-19 22:13:48 -0700

    Program that prints _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c
+ * @c: character to print
+ * Return: Always 1 (Success)
+ */
+
+int _putchar(char c)
+{
+	return (write(i, &c, 1));
+}
